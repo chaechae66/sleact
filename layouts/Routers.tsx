fdcc780534af33a25b2e8448855bba63815ff6f7
chaechae66 <valueworkerchae@gmail.com>
2023-08-31 12:20:38 +0900

    Feat: 로그인 후 워크스페이스 & 채널 구현

--- layouts/Routers.tsx
+++ layouts/Routers.tsx
@@ -1,23 +1,28 @@
-import { createBrowserRouter } from "react-router-dom";
-import React from "react";
+import { createBrowserRouter } from 'react-router-dom';
+import React from 'react';
 import loadable from '@loadable/component';
+import Channel from '@pages/Channel';
 
-const SignUp = loadable(()=>import("@pages/SignUp"));
-const LogIn = loadable(()=>import("@pages/LogIn"));
+const SignUp = loadable(() => import('@pages/SignUp'));
+const LogIn = loadable(() => import('@pages/LogIn'));
 
 const Routers = createBrowserRouter([
-    {
-        path:"/",
-        element:<LogIn />
-    },
-    {
-        path:"/login",
-        element : <LogIn />
-    },
-    {
-        path:"/signup",
-        element : <SignUp />
-    },
+  {
+    path: '/',
+    element: <LogIn />,
+  },
+  {
+    path: '/login',
+    element: <LogIn />,
+  },
+  {
+    path: '/signup',
+    element: <SignUp />,
+  },
+  {
+    path: '/workspace/channel',
+    element: <Channel />,
+  },
 ]);
 
-export default Routers
+export default Routers;
